e4c4a50e8c6948df88cdff1fa5511fe1e565c213
milworm <ruslan.prytula@gmail.com>
2015-11-17 23:23:17 -0500

    worked on landing page

--- js/app.jsx
+++ js/app.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import LanguageSwitch from "./components/LanguageSwitch.jsx!";
 import Translatable from "./mixins/Translatable.jsx!";
+import Content from "./views/Content.jsx!";
 
 export default React.createClass({
     mixins: [Translatable],
     render: function() {
         return (
             <div className="layout">
-                <div className="layout-inner">
-                    <LanguageSwitch />
-                </div>
+                <LanguageSwitch />
+                <Content />
             </div>
         );
     }
